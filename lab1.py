dd0fce3060d0841572538cd7c52535fb7b4e55e8
Mohammed Hussain <101076328+mh575@users.noreply.github.com>
2023-09-11 18:30:37 -0600

    test

--- lab1.py
+++ lab1.py
@@ -1,6 +1,11 @@
 import requests
+import os
 
-url = "http://www.google.com/"
+# url = "http://www.google.com/"
+# res = requests.get(url)
+# print(res)
+
+url = "https://raw.githubusercontent.com/mh575/cmput404_lab1/master/lab1.py"
 res = requests.get(url)
-print(res)
+print(res.text)
 
